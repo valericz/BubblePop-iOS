4932bc1b4a34caadcf6c4ec1f6755261227a57a4
Valerie Zhang <valerieznb@gmail.com>
2025-04-20 20:13:12 +1000

    added explosion effect

---
+++ BubblePop/ExplosionView.swift
@@ -0,0 +1,18 @@
+//
+//  ExplosionView.swift
+//  BubblePop
+//
+//  Created by WEIHUA ZHANG on 20/4/2025.
+//
+
+import SwiftUI
+
+struct ExplosionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExplosionView()
+}
